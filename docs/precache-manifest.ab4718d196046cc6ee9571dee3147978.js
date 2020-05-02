a4bc7547452bda605e6888eb6345d92c649035b4
Luis Tadeo <letsalcido@gmail.com>
2020-05-02 01:24:25 -0700

    build under docs

---
+++ docs/precache-manifest.ab4718d196046cc6ee9571dee3147978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dec3defb7aba18006b891cd291f2928",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d76725b16d0ef5dc59c",
+    "url": "/static/css/main.93d3fbd1.chunk.css"
+  },
+  {
+    "revision": "8cfab60d0c323ff33c58",
+    "url": "/static/js/2.8d6ba80a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8d6ba80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d76725b16d0ef5dc59c",
+    "url": "/static/js/main.b95feb8e.chunk.js"
+  },
+  {
+    "revision": "0b4f3269348c2e5315e5",
+    "url": "/static/js/runtime-main.9089ee33.js"
+  }
+]);
